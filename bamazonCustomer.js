09f309515b28b924cd3704048a02f10f6b136a76
Michael Vega <ichigo509@yahoo.com>
2019-08-23 23:48:14 -0400

    Finished both parts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,33 +34,69 @@ var readTable = function () {
         for (let i = 0; i < res.length; i++) {
             console.log("Id: " + res[i].id + ", Product Name: " + res[i].product_name + ", Price: $" + res[i].price + "\n");
         }
+        askUser();
     });
+}
+var updateTable = function (quantity, id) {
+    connection.query("UPDATE products SET ? WHERE ?",
+        [{
+            stock_quantity: quantity
+        },
+        {
+            id: id
+        }
+        ], function (err) {
+            if (err) throw err;
+        }
+    )
 }
 //   The app should then prompt users with two messages.
 var askUser = function () {
-    inquirer
-        // The first should ask them the ID of the product they would like to buy.
-        .prompt([
-            {
-                type: "input",
-                message: "Which product would you like to buy? (ID)",
-                name: "buy"
-            },
-            // The second message should ask how many units of the product they would like to buy.
-            {
-                type: "input",
-                message: "How many units of the product would you like to buy?",
-                name: "quantity"
-            }
-        ])
-    // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-        .then(function(res){
-            
-        })
-    // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-    // However, if your store does have enough of the product, you should fulfill the customer's order.
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        inquirer
+            // The first should ask them the ID of the product they would like to buy.
+            .prompt([
+                {
+                    type: "input",
+                    message: "Which product would you like to buy? (ID)",
+                    name: "buy"
+                },
+                // The second message should ask how many units of the product they would like to buy.
+                {
+                    type: "input",
+                    message: "How many units of the product would you like to buy?",
+                    name: "quantity"
+                }
+            ])
+            // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+            .then(function (answer) {
+                //make sure to put NaN for answer.buy and answer.quantity
+                var chosenItem;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].id === parseInt(answer.buy)) {
+                        chosenItem = res[i];
+                    }
+                }
+                // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+                // However, if your store does have enough of the product, you should fulfill the customer's order.
+                if (chosenItem.stock_quantity < answer.quantity) {
+                    console.log("Sorry we don't have enough units to sell you")
+                } else {
+                    console.log("Current stock: " + chosenItem.stock_quantity);
+                    // This means updating the SQL database to reflect the remaining quantity.
+                    var newQuantity = chosenItem.stock_quantity - parseInt(answer.quantity);
+                    updateTable(newQuantity, parseInt(answer.buy));
+                    console.log("Item(s) bought!!!\nNew Quantity: " + chosenItem.stock_quantity);
+                    // Once the update goes through, show the customer the total cost of their purchase.
+                    console.log("\nTotal cost of purchase: $" + (chosenItem.price * parseInt(answer.quantity)));
+                }
+                connection.end();
 
-    // This means updating the SQL database to reflect the remaining quantity.
-    // Once the update goes through, show the customer the total cost of their purchase.
+            })
+
+
+
+    })
 }
 
---
+++ bamazonManager.js
@@ -0,0 +1,180 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Hearthstone5174",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    viewCommands();
+});
+var viewCommands = function () {
+    // List a set of menu options:
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"],
+                message: "What would you like to do?",
+                name: "command"
+            }
+        ])
+        .then(function (answer) {
+            switch (answer.command) {
+                case "View Products for Sale":
+                    viewProducts();
+                    break;
+                case "View Low Inventory":
+                    viewLow();
+                    break;
+                case "Add to Inventory":
+                    addInventory();
+                    break;
+                case "Add New Product":
+                    newProduct();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+                default:
+                    console.log("No command was chosen");
+            }
+        })
+}
+
+
+// If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
+function viewProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        for (let i = 0; i < res.length; i++) {
+            console.log("Id: " + res[i].id + ", Product Name: " + res[i].product_name + ", Price: $" + res[i].price + "\n");
+        }
+        viewCommands();
+    });
+}
+// If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+function viewLow() {
+    console.log("Selecting all products with a low count...\n");
+    connection.query("SELECT * FROM products WHERE stock_quantity <=?", [50], function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        for (let i = 0; i < res.length; i++) {
+            console.log("Id: " + res[i].id + ", Product Name: " + res[i].product_name + ", Stock: " + res[i].stock_quantity + " units\n");
+        }
+        viewCommands();
+    });
+}
+// If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+function addInventory() {
+    var chosenItem;
+    connection.query("SELECT * FROM products ", function (err, res) {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].product_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which one of these items would you like to add more to?",
+                    name: "choice"
+                },
+                {
+                    type: "input",
+                    message: "How many more units would you like to add?",
+                    name: "add"
+                }
+            ])
+            .then(function (ans) {
+                
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].product_name === ans.choice) {
+                        chosenItem = res[i];
+                    }
+                }
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [{
+                        stock_quantity: (chosenItem.stock_quantity + parseInt(ans.add))
+                    },
+                    {
+                        id: chosenItem.id
+                    }
+                    ], function (err) {
+                        if (err) throw err;
+                    }
+                )
+            }).then(function () {
+                console.log("\nNew stock: " + chosenItem.stock_quantity);
+                viewCommands();
+            })
+
+    })
+}
+// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
+function newProduct() {
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "input",
+                message: "What is the item you would like to submit?"
+            },
+            {
+                name: "department",
+                type: "input",
+                message: "What category would you like to place your auction in?"
+            },
+            {
+                name: "price",
+                type: "input",
+                message: "What would you like the price to be?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "stock",
+                message: "How many units would you like to initialize?",
+                type: "input"
+            }
+        ])
+        .then(function (answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                "INSERT INTO products SET ?",
+                {
+                    product_name: answer.item,
+                    department_name: answer.department,
+                    price: answer.price || 0,
+                    stock_quantity: answer.stock || 0
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your item was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    viewCommands();
+                }
+            );
+        });
+}
